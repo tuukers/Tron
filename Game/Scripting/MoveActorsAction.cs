8756760da6fbf15d042643aa699c9738c51cf89a
tuukers <ethangraham321abc@gmail.com>
2022-06-15 10:57:02 -0600

    first commit

---
+++ Game/Scripting/MoveActorsAction.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Tron.Game.Casting;
+using Tron.Game.Services;
+
+
+namespace Tron.Game.Scripting
+{
+    /// <summary>
+    /// <para>An update action that moves all the actors.</para>
+    /// <para>
+    /// The responsibility of MoveActorsAction is to move all the actors.
+    /// </para>
+    /// </summary>
+    public class MoveActorsAction : Action
+    {
+        /// <summary>
+        /// Constructs a new instance of MoveActorsAction.
+        /// </summary>
+        public MoveActorsAction()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Execute(Cast cast, Script script)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            foreach (Actor actor in actors)
+            {
+                actor.MoveNext();
+            }
+        }
+    }
+}
