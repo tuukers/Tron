45453feae2972beeff9efbbaf18ffb07f6fcd092
tuukers <ethangraham321abc@gmail.com>
2022-06-21 10:19:57 -0600

    handle collisions ready

--- Game/Scripting/HandleCollisionsAction.cs
+++ Game/Scripting/HandleCollisionsAction.cs
@@ -30,7 +30,7 @@ namespace Tron.Game.Scripting
         {
             if (isGameOver == false)
             {
-                HandleFoodCollisions(cast);
+                //HandleFoodCollisions(cast);
                 HandleSegmentCollisions(cast);
                 HandleGameOver(cast);
             }
@@ -40,20 +40,20 @@ namespace Tron.Game.Scripting
         /// Updates the score nd moves the food if the snake collides with it.
         /// </summary>
         /// <param name="cast">The cast of actors.</param>
-        private void HandleFoodCollisions(Cast cast)
-        {
-            Snake snake = (Snake)cast.GetFirstActor("snake");
-            Score score = (Score)cast.GetFirstActor("score");
-            Food food = (Food)cast.GetFirstActor("food");
+        // private void HandleFoodCollisions(Cast cast)
+        // {
+        //     Snake snake = (Snake)cast.GetFirstActor("snake");
+        //     Score score = (Score)cast.GetFirstActor("score");
+        //     Food food = (Food)cast.GetFirstActor("food");
             
-            if (snake.GetHead().GetPosition().Equals(food.GetPosition()))
-            {
-                int points = food.GetPoints();
-                snake.GrowTail(points);
-                score.AddPoints(points);
-                food.Reset();
-            }
-        }
+        //     if (snake.GetHead().GetPosition().Equals(food.GetPosition()))
+        //     {
+        //         int points = food.GetPoints();
+        //         snake.GrowTail(points);
+        //         score.AddPoints(points);
+        //         food.Reset();
+        //     }
+        // }
 
         /// <summary>
         /// Sets the game over flag if the snake collides with one of its segments.
@@ -61,15 +61,57 @@ namespace Tron.Game.Scripting
         /// <param name="cast">The cast of actors.</param>
         private void HandleSegmentCollisions(Cast cast)
         {
-            Snake snake = (Snake)cast.GetFirstActor("snake");
-            Actor head = snake.GetHead();
-            List<Actor> body = snake.GetBody();
+            Player player1 = (Player)cast.GetFirstActor("player1");
+            Player player2 = (Player)cast.GetFirstActor("player2");
+            Actor head1 = player1.GetHead();
+            Actor head2 = player2.GetHead();
+            List<Actor> body1 = player1.GetBody();
+            List<Actor> body2 = player2.GetBody();
 
-            foreach (Actor segment in body)
+            int x = Constants.MAX_X / 2;
+            int y = Constants.MAX_Y / 2;
+            Point position = new Point(x, y);
+
+            if (isGameOver == false)
             {
-                if (segment.GetPosition().Equals(head.GetPosition()))
+                foreach (Actor segment in body1)
                 {
-                    isGameOver = true;
+                    if (segment.GetPosition().Equals(head1.GetPosition()))
+                    {
+                        isGameOver = true;
+
+                        Actor message = new Actor();
+                        message.SetText("Game Over!");
+                        message.SetPosition(position);
+                        cast.AddActor("messages", message);
+                    }
+                    if (segment.GetPosition().Equals(head2.GetPosition()))
+                    {
+                        isGameOver = true;
+                        Actor message = new Actor();
+                        message.SetText("Player 1 Wins!!!");
+                        message.SetPosition(position);
+                        cast.AddActor("messages", message);
+                    }
+                }
+                foreach (Actor segment in body2)
+                {
+                    if (segment.GetPosition().Equals(head1.GetPosition()))
+                    {
+                        isGameOver = true;
+                        Actor message = new Actor();
+                        message.SetText("Player 2 Wins!!!");
+                        message.SetPosition(position);
+                        cast.AddActor("messages", message);
+                    }
+                    if (segment.GetPosition().Equals(head2.GetPosition()))
+                    {
+                        isGameOver = true;
+                        Actor message = new Actor();
+                        message.SetText("Player 1 Wins!!");
+                        message.SetPosition(position);
+                        cast.AddActor("messages", message);
+                    }
                 }
             }
         }
@@ -78,26 +120,32 @@ namespace Tron.Game.Scripting
         {
             if (isGameOver == true)
             {
-                Snake snake = (Snake)cast.GetFirstActor("snake");
-                List<Actor> segments = snake.GetSegments();
-                Food food = (Food)cast.GetFirstActor("food");
+                Player player2 = (Player)cast.GetFirstActor("player2");
+                Player player1 = (Player)cast.GetFirstActor("player1");
+                List<Actor> segments1 = player1.GetSegments();
+                List<Actor> segments2 = player2.GetSegments();
+                //Food food = (Food)cast.GetFirstActor("food");
 
                 // create a "game over" message
                 int x = Constants.MAX_X / 2;
                 int y = Constants.MAX_Y / 2;
                 Point position = new Point(x, y);
 
-                Actor message = new Actor();
-                message.SetText("Game Over!");
-                message.SetPosition(position);
-                cast.AddActor("messages", message);
+                // Actor message = new Actor();
+                // message.SetText("Game Over!");
+                // message.SetPosition(position);
+                // cast.AddActor("messages", message);
 
                 // make everything white
-                foreach (Actor segment in segments)
+                foreach (Actor segment in segments1)
                 {
                     segment.SetColor(Constants.WHITE);
                 }
-                food.SetColor(Constants.WHITE);
+                foreach (Actor segment in segments2)
+                {
+                    segment.SetColor(Constants.WHITE);
+                }
+                //food.SetColor(Constants.WHITE);
             }
         }
 
